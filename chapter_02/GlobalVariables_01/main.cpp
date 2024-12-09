a711b5a4382922b149c3ec2813448b75c2c1b8d1
Omraj Rajan <omrajrajan@gmail.com>
2024-12-09 15:56:17 +0530

    added chapter 2 and new project 'GlobalVariables' to it
    added chapter 2 and new project 'GlobalVariables' to it

---
+++ chapter_02/GlobalVariables_01/main.cpp
@@ -0,0 +1,15 @@
+/*
+Program to demonstrate Global in C++
+
+Programmer: Omi
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Program to demonstrate ... in C++" << endl;
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
