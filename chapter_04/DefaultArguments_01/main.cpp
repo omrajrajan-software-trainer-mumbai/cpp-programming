11f2732f9fd2d945827ae93599e495d042a7961b
Omraj Rajan <omrajrajan@gmail.com>
2024-12-24 13:59:52 +0530

    new projects in chapter 04

---
+++ chapter_04/DefaultArguments_01/main.cpp
@@ -0,0 +1,52 @@
+// Creating a program to demonstrate an example of default arguments in C++ with CodeBlocks
+// Chapter 04 Page no.76
+
+// Preprocessor Directives
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+// The application starts with the main ()
+int main()
+{
+    cout << "Creating a program to demonstrate an example of default arguments in C++ with CodeBlocks\n" << endl;
+
+    // Variable Declaration and Initialization
+    float amount;
+    // Declaring a function within the main()
+    float value (float first_parameter, int integer_parameter, default_parameter = 0.15);
+    void printline (char symbol = '*', length = 40);
+
+    // Calling the function  and it uses the default values for arguement
+    printline();
+
+    // Calling the function and using the default value for the third parameter
+    amount = value (5000.00, 5);
+    cout << "\nFinal Value = " << amount << "\n\n";
+
+    // Passing all the values explicitly
+    amount = value (10000.00, 5,  0.30)
+    cout << "\nFinal Value = " << amount << "\n\n";
+
+    // Use default value for second arguement
+    printline ('=');
+
+    return 0;
+}
+
+// Defining the functions
+float value (float first_parameter, int integer_parameter, default_parameter)
+{
+    int year = 1;
+    float sum = first_parameter;
+
+    while (year <= integer_parameter)
+    {
+        sum = sum * (1 + default_parameter);
+        year++;
+    }
+    return (sum);
+}
+void printline
+
---
+++ chapter_04/InlineFunctions_01/main.cpp
@@ -0,0 +1,35 @@
+// Creating a program to demonstrate the use of inline functions in C++ with CodeBlocks
+// Chapter 04 Page no.74
+
+// Preprocessor Directives
+#include <iostream>
+
+using namespace std;
+
+// Defining an inline function named multiply
+inline float multiply (float first_parameter, float second_parameter)
+{
+    return first_parameter * second_parameter;
+}
+
+// Defining an inline function named divide
+inline float divide (float first_parameter, float second_parameter)
+{
+    return first_parameter / second_parameter;
+}
+
+// The application starts with the main ()
+int main()
+{
+    cout << "Creating a program to demonstrate the use of inline functions in C++ with CodeBlocks\n" << endl;
+
+    // Variable Declaration and Initialization
+    float first_value = 12.345;
+    float second_value = 9.82;
+
+    // Displaying the output
+    cout << "The product of both the values are "<< multiply(first_value, second_value) << "\n";
+    cout << "The remainder of both the values are "<< divide(first_value, second_value) << "\n";
+
+    return 0;
+}
