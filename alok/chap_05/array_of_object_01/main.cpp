84b519e462cfb10b853f36a4e27d2c07da075eaf
alok-yadav-programmer-mumbai <aloky3974@gmail.com>
2025-01-02 11:35:14 +0530

    projects added to chapter 5 in alok folder
    projects added to chapter 5 in alok folder

---
+++ alok/chap_05/array_of_object_01/main.cpp
@@ -0,0 +1,65 @@
+// Creating a program to demonstrate an example of arrays within a class and also as objects array in C++ with CodeBlocks
+// Chapter 05 Page no.110
+
+// Preprocessor Directives
+#include <iostream>
+
+using namespace std;
+
+// Creating a class named employee
+class employee
+{
+    private:
+        char name[30];
+        float age;
+    public:
+        void get_data(void);
+        void put_data(void);
+};
+
+// Defining the function get_data
+void employee ::get_data(void)
+{
+    cout << "\nEnter the name of the employee: ";
+    cin >> name;
+    cout << "Enter the age of the employee: ";
+    cin >> age;
+}
+
+// Defining the function put_data
+void employee ::put_data()
+{
+    cout << "Name: " << name << "\n";
+    cout << "Age: " << age << "\n";
+}
+
+// Defining the size of array as constant
+const int SIZE = 3;
+
+// The application starts with the main() function
+int main()
+{
+    cout << "Creating a program to demonstrate an example of arrays within a class and also as objects array in C++ with CodeBlocks\n" << endl;
+
+    // Declaring the object
+    employee manager[SIZE];
+
+    // Initiating a loop to input data from user
+    for(int loop_counter = 0; loop_counter < SIZE; loop_counter++)
+    {
+        cout << "\nDetails of Manager" << loop_counter + 1 << "\n";
+        // Calling the function to get data from the user
+        manager[loop_counter].get_data();
+    }
+    cout << "\n";
+
+    // Initiating a loop to display data
+    for(int loop_counter = 0; loop_counter < SIZE; loop_counter++)
+    {
+        cout << "\nManager" << loop_counter + 1 << "\n";
+        // Calling the function to display the data
+        manager[loop_counter].put_data();
+    }
+
+    return 0;
+}
---
+++ alok/chap_05/objects_as_arguments_01/main.cpp
@@ -0,0 +1,44 @@
+// Creating a program to demonstrate an example of using objects as arguments in C++ with CodeBlocks
+// Chapter 05 Page no.111
+
+// Preprocessor Directives
+#include <iostream>
+
+using namespace std;
+
+// Creating a class named time
+class Time
+{
+    private:
+        int hours;
+        int minutes;
+    public:
+        void get_time(int hour_parameter, int minute_parameter)
+        {
+            hours = hour_parameter;
+            minutes = minute_parameter;
+        }
+        void put_time(void)
+        {
+            cout << hours << "hours and ";
+            cout << minutes << "minutes \n";
+        }
+        // Function declaration with objects as arguments
+        void sum(Time, Time);
+};
+
+// Defining the function sum()
+// Here time_1 and time_2 are objects
+void Time ::sum(Time time_1, Time time_2)
+{
+    minutes = time_1.minutes + time_2.minutes;
+    hours = minutes / 60;
+
+}
+
+// The application starts with the main ()
+int main()
+{
+    cout << "Creating a program to demonstrate an example of using objects as arguments in C++ with CodeBlocks\n" << endl;
+    return 0;
+}
