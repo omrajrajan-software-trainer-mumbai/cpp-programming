d0d83f9fa49947e56f52e0f9159663e572a745a2
alok-yadav-programmer-mumbai <aloky3974@gmail.com>
2025-01-06 11:57:06 +0530

    project added to chap 2 in alok folder
    project added to chap 2 in alok folder

---
+++ alok/chap_02/printing_a_string_01/main.cpp
@@ -0,0 +1,14 @@
+// Program to print a string in C++ with CodeBlocks
+// chap 02 page no 04
+
+// Preprocessor directive
+#include <iostream>
+
+using namespace std;
+
+// Application starts with the main() function
+int main()
+{
+    cout << "C++ is better than C.\n";
+    return 0;
+}
