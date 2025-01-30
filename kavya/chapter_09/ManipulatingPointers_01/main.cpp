556f91a20a01c833cf12e5da15b5f5523a311182
Omraj Rajan <omrajrajan@gmail.com>
2025-01-30 20:12:12 +0530

    updates to chap 09
    updates to chap 09

--- kavya/chapter_09/ManipulatingPointers_01/main.cpp
+++ kavya/chapter_09/ManipulatingPointers_01/main.cpp
@@ -1,4 +1,4 @@
-// Creating a program to demonstrate an example of Pointer in C++ with CodeBlocks
+// Creating a program to demonstrate of Pointer Manipulation in C++ with CodeBlocks
 // Chapter 09 Page no.225
 
 // Preprocessor Directives
@@ -10,7 +10,7 @@ using namespace std;
 // The application starts with the main ()
 int main()
 {
-    cout << "Creating a program to demonstrate an example of Pointer in C++ with CodeBlocks\n" << endl;
+    cout << "Creating a program to demonstrate of Pointer Manipulation in C++ with CodeBlocks" << endl << endl;
 
     // Variable, Pointer Declaration and Initialization
     int variable_1 = 10;
@@ -20,7 +20,7 @@ int main()
     cout << "The value of variable_1 is " << *pointer_variable;
     *pointer_variable = *pointer_variable + variable_1;
 
-    cout << "\n\nThe revised value of variable_1 is " << variable_1 << "\n\n";
+    cout << "\n\nThe revised value of variable_1 is " << variable_1 << endl << endl;
 
     return 0;
 }
--- kavya/chapter_09/PointerExample_01/main.cpp
+++ kavya/chapter_09/PointerExample_01/main.cpp
@@ -8,24 +8,27 @@
 using namespace std;
 
 // The application starts with the main ()
-int main()
-{
+int main() {
     cout << "Creating a program to demonstrate an example of Pointer in C++ with CodeBlocks\n" << endl;
 
     // Variable, Pointer Declaration and Initialization
-    int first_variable, *first_pointer, **second_pointer;
-    first_pointer = &first_variable;
-    second_pointer = &first_pointer;
+    int first_variable = 10, *first_pointer, **second_pointer;
+    first_pointer = &first_variable; // store the address of the first_variable
+    second_pointer = &first_pointer; // store the address of the first_pointer
 
-    cout << "Before incrementing the address values:\n";
-    cout << "The address of the first_variable is " << first_pointer << "\n";
-    cout << "The address of the first_pointer is " << second_pointer << "\n\n";
+    cout << "Before incrementing the address values:" << endl;
+    cout << "The address of the first_variable is " << first_pointer << endl;
+    cout << "The address of the first_pointer is " << second_pointer << endl << endl;
 
-    cout << "After incrementing the address values:\n";
+    cout << "The value of the first_variable is " << *first_pointer << endl;
+    cout << "The address of the first_variable is " << *second_pointer << endl ;
+    cout << "The value of the first_variable is " << **second_pointer << endl << endl;
+
+    cout << "After incrementing the address values:"<< endl;
     first_pointer += 2;
-    cout << "The address of the first_variable is " << first_pointer << "\n";
+    cout << "The address of the first_variable is " << first_pointer << endl;
     second_pointer += 2;
-    cout << "The address of the first_pointer is " << second_pointer << "\n\n";
+    cout << "The address of the first_pointer is " << second_pointer << endl << endl;
 
     return 0;
 }
