d63c7cbd35d202ec888db6f44cd5be59f44d0932
Omraj Rajan <omrajrajan@gmail.com>
2025-02-03 17:50:41 +0530

    changes by omi
    changes by omi

---
+++ chapter_09/array_of_pointer_01/main.cpp
@@ -0,0 +1,43 @@
+// Program to demonstrate arrays of pointers in C++
+// Chap 09  Page no 14
+
+#include <iostream>
+// For strcmp()
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int loop_counter = 0;
+
+    // Array of pointers to strings
+    char *array_pointer[4] =
+    {
+        "books",
+        "television",
+        "computer",
+        "sports"
+    };
+
+    char string_to_search[25];
+
+    cout << "Enter your favorite leisure pursuit: ";
+
+    // Using getline to prevent overflow
+    cin.getline(string_to_search, 25);
+
+    for (loop_counter = 0; loop_counter < 4; loop_counter++)
+    {
+        // Removed unnecessary dereferencing
+        if (!strcmp(string_to_search, array_pointer[loop_counter]))
+        {
+            cout << endl << "Your favorite pursuit is available here." << endl;
+            break;
+        }
+    }
+    if (loop_counter == 4)
+        cout << endl << "Your favorite pursuit is not available here." << endl;
+
+    // Using standard return statement instead of getch()
+    return 0;
+}
